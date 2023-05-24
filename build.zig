248fcfba415dc2003ef4bb8bc44483087d253509
Quentin_T <quentin.tessier@epitech.eu>
2023-05-24 15:56:40 +0200

    Modifying the way we get the current directory for compilation

--- build.zig
+++ build.zig
@@ -1,5 +1,9 @@
 const std = @import("std");
 
+inline fn thisDir() []const u8 {
+    return comptime std.fs.path.dirname(@src().file) orelse ".";
+}
+
 pub const zFont = struct {
     zfont: *std.Build.Module,
 
@@ -10,7 +14,7 @@ pub const zFont = struct {
 
 pub fn package(b: *std.Build, _: std.zig.CrossTarget, _: std.builtin.Mode) zFont {
     const zfont = b.createModule(.{
-        .source_file = .{ .path = comptime std.fs.path.dirname(@src().file) orelse "." ++ "/src/font.zig" },
+        .source_file = .{ .path = thisDir() ++ "/src/font.zig" },
         .dependencies = &.{},
     });
     return zFont{ .zfont = zfont };
